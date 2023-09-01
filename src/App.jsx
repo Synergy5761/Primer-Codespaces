3b9581d757947c903a75d7b77f40cc06fc11e88a
Jeff <135196190+Synergy5761@users.noreply.github.com>
2023-09-01 04:06:04 +0000

    Updated App.jsx to have name and personal information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  name: "Jeff Tang",
+  title: "Computer Engineering Student",
+  email: "jeff@example.com",
+  gitHub: "https://github.com/Synergy5761",
+  instagram: "",
   linkedIn: "satyanadella",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
