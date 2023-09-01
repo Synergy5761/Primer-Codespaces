55bb45a6d6d4c4c434449a48598dcaebebbc03a1
Jeff <135196190+Synergy5761@users.noreply.github.com>
2023-09-01 05:19:30 +0000

    Personalized About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,24 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/earth-from-space.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "Space view of Earth during night time";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a Computer Engineering student studying at California State Polytechnic University, Pomona.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Circuit Analysis",
+  "Digital Logic Design",
+  "GitHub"
 ];
 
 /**
@@ -45,7 +42,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Lifelong learner for all technology.";
 
 const About = () => {
   return (
@@ -53,8 +50,8 @@ const About = () => {
       <img className="background" src={image} alt={imageAltText} />
       <div
         style={{
-          backgroundColor: "white",
-          width: "50%",
+          backgroundColor: "gray",
+          width: "30%",
           padding: "4rem",
           margin: "3rem auto",
           textAlign: "center",
@@ -66,7 +63,7 @@ const About = () => {
         <ul
           style={{
             textAlign: "left",
-            columns: 2,
+            columns: 1,
             fontSize: "1.25rem",
             margin: "2rem 3rem",
             gap: "3rem",
