82ede9997c24b6c441bda5c756d070ebe0ee27eb
Jeff <135196190+Synergy5761@users.noreply.github.com>
2023-09-01 05:02:39 +0000

    Changed color for name and title to white

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,15 +22,17 @@ import PropTypes from "prop-types";
  */
 import image from "../images/code-on-laptop.jpg";
 
-const imageAltText = "On the screen of a laptop, showcasing code";
+const imageAltText = "Screen of a laptop, showing code";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
       <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
-        <h1>{name}</h1>
-        <h2>{title}</h2>
+        <div style={{ color: "white" }}>
+          <h1>{name}</h1>
+          <h2>{title}</h2>
+        </div>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
