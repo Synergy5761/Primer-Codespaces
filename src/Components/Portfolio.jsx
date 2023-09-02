b847b27625a7df35f5ed994eb4eb693a4875c33f
Jeff <135196190+Synergy5761@users.noreply.github.com>
2023-09-02 05:47:00 +0000

    attempt to have the background of Portfolio.jsx gray

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -60,6 +60,13 @@ const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+
+       <div className="background"> 
+        <div style={{color: "gray"}}>
+
+        </div>
+      </div>
+
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
